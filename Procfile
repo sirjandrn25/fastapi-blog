ffcf8ea92e53db63a1dfa3effb888b6872fe71ad
sirjandrn25 <sirjantmg99@gmail.com>
2021-11-24 22:14:13 +0545

    add Procfile and change database for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+uvicorn --port 5000 --host 127.0.0.1 src.main:app --reload
--- src/database.py
+++ src/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:meronaam54@localhost/blog_fastapi"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:meronaam54@localhost/blog_fastapi"
+SQLALCHEMY_DATABASE_URL="postgresql://wimurfeprqdbde:6408e1bd2aca6118bc2e3fc987e8291cd03f3645986e00b4f185bef3dfaa9d38@ec2-75-101-141-195.compute-1.amazonaws.com:5432/d4gsjofmrjcl5h"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
